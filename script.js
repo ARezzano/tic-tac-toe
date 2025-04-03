4d4342409274ef97048821da1be1a271d48aa860
Alejandro Rezzano <rezzanoalejandro@gmail.com>
2025-04-03 14:51:06 -0300

    In gameManager, create player objects, win conditions and game winning/drawing logic

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const Gameboard = (()=>{
-    const board = [null,null,null];
+    const board = [null,null,null,null,null,null,null,null,null];
     return {getBoard: ()=> [...board]}
 })();
 
@@ -8,5 +8,41 @@ function newPlayer(name,marker){
 }
 
 const gameManager = (() => {
-    
+    const player1 = newPlayer("Player 1", "X");
+    const player2 = newPlayer("Player 2", "O");
+    let currentTurn = player1;
+
+    const winConditions = () => {
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,4,8],
+        [2,4,6],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8]
+    }
+
+    const getWinner = () => {
+        const board = Gameboard.getBoard();
+
+        for(const condition of winConditions){
+            const [a,b,c] = condition;
+
+            if(board[a] && board[a] === board[b] && board[a] === board[c]){
+                return board[a] === player1.marker ? player1 : player2;
+            }
+
+            if(!board.includes(null)){
+                return "draw";
+            }
+
+            return null;
+        }
+
+        return{
+            currentTurn,
+            getWinner
+        }
+    }
 })();
